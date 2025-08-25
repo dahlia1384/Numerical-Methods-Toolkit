967e83c50ef2afc8b2eaa99fb3f94c73c0778b5e
Dahlia Mohammadi <mohammadi.dahlia@gmail.com>
2025-08-25 16:38:47 -0700

    Create linearalgebra.hpp

---
+++ include/nmtk/linearalgebra.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Eigen/Core>
+#include <tuple>
+
+namespace nmtk {
+
+// Conjugate Gradient for SPD A
+std::tuple<Eigen::VectorXd,int,double>
+cg(const Eigen::MatrixXd& A, const Eigen::VectorXd& b,
+   int max_iters = 1000, double tol = 1e-10);
+
+} // dahlia1384
